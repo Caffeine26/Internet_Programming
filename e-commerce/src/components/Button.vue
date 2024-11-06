c485118980de3355737f731b3df9b6621c10d27e
sonit <sorsonit06@gmail.com>
2024-11-06 22:06:41 +0700

    Finished TP1

---
+++ e-commerce/src/components/Button.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="Container">
+        <span class="text">Shop Now</span> 
+        <i class="fa-solid fa-arrow-right"></i>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    .Container{
+        display: flex;
+        align-items: center;
+        height: 30px;
+        width: 100px;
+        color: white;
+        font-size: 10px;
+        font-weight: bold;
+        background-color: #3BB77E;
+        border-radius: 5px;
+        padding:3px 0px;
+        padding-left: 15px;
+    }
+
+    .text{
+        display: flex;
+        align-items: center;
+        padding:10px;
+    }
+    .icon{
+        width: 10px;
+        color: white;
+    }
+</style>
